1e85d8886a353ee96f625e67c5a58bd5eccab1fe
yanfunnyboy1 <115864305+yanfunnyboy1@users.noreply.github.com>
2024-10-18 15:51:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `Hello ${tg.initDataUnsafe.user.first_name} !`;
 
 usercard.appendChild(p);
