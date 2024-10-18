7117accb6ad1f46c3fcec5e34b31aba5975d2e55
yanfunnyboy1 <115864305+yanfunnyboy1@users.noreply.github.com>
2024-10-18 18:24:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let usercard = document.getElementById("usercard")
 
 let p = document.createElement('p');
 
-p.innerText = `${tg.initDataUnsafe.first_name}`; //показываем user_id
+p.innerText = `${tg.initDataUnsafe.user.username}`; //показываем user_id
 usercard.appendChild(p); //добавляем
 var link = "ton://transfer/UQBtGBmUDj8trZTobcT7nYCfigF7R9JSi9sTxrR0zL0eAm6B?amount=500000000&text="+ tg.initDataUnsafe.user;
 let btn1 = document.getElementById("btn1")
